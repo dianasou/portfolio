854922374e41f590a7f2cf359de0073cabaf75c9
dianasou <dsouthiphomma@gmail.com>
2021-03-23 17:03:55 -0500

    Add menuToggle function to use menu

--- js/script.js
+++ js/script.js
@@ -2,3 +2,14 @@ console.log("Hello world");
 
 //personal message to readers
 console.log("This is Diana. Welcome to my portfolio!");
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    //add responsive to className to show menu
+    x.className += ' responsive';
+  } else {
+    //change className back to hide menu
+    x.className = 'navtoggle';
+  }
+}
