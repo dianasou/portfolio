ad8d9c17296da7f32ece43870c7b0f98370adfd6
dianasou <dsouthiphomma@gmail.com>
2021-03-23 16:53:50 -0500

    Revert "Add menuToggle to show menu"
    This reverts commit be73cb1311b9f94fb48bfc46f952bacb07865d6d.

--- js/script.js
+++ js/script.js
@@ -2,14 +2,3 @@ console.log("Hello world");
 
 //personal message to readers
 console.log("This is Diana. Welcome to my portfolio!");
-
-function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
-  if (x.className === 'navtoggle') {
-    //add responsive to classname if menu isn't shown to show menu
-    x.className += ' responsive';
-  } else {
-    //change className back to navtoggle
-    x.className = 'navtoggle';
-  }
-}
