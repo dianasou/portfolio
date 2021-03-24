8672b8891b74a82b90e2b06e236b4545b4eb1c32
dianasou <dsouthiphomma@gmail.com>
2021-03-24 18:17:24 -0500

    Add side nav

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ console.log("Hello world");
 //personal message to readers
 console.log("This is Diana. Welcome to my portfolio!");
 
+// menuToggle function shows nav when clicked
 function menuToggle() {
   var x = document.getElementById('myNavtoggle');
   if (x.className === 'navtoggle') {
@@ -13,3 +14,29 @@ function menuToggle() {
     x.className = 'navtoggle';
   }
 }
+
+//return node list that match group of selectors for side nav
+let mainNavLinks = document.querySelectorAll(".sidenav ul li a");
+let mainSections = document.querySelectorAll("main section");
+
+let lastId;
+let cur = [];
+
+//side nav to scroll to section
+window.addEventListener("scroll", event => {
+  let fromTop = window.scrollY;
+
+  mainNavLinks.forEach(link => {
+    let section = document.querySelector(link.hash);
+
+    if (
+      section.offsetTop <= fromTop &&
+      section.offsetTop + section.offsetHeight > fromTop
+    ) {
+      link.classList.add("current");
+    } else {
+      link.classList.remove("current");
+    }
+  });
+
+});
