212eed3c30b91634541f0629e3d24cb50935f783
dianasou <dsouthiphomma@gmail.com>
2021-03-24 21:59:39 -0500

    Revert "Change navtoggle class to navbar"
    This reverts commit 03ac6861d8d4a1323508ab7d7f1e509f1a15ff04.

--- js/script.js
+++ js/script.js
@@ -6,12 +6,12 @@ console.log("This is Diana. Welcome to my portfolio!");
 // menuToggle function shows nav when clicked
 function menuToggle() {
   var x = document.getElementById('myNavtoggle');
-  if (x.className === 'navbar') {
+  if (x.className === 'navtoggle') {
     //add responsive to className to show menu
     x.className += ' responsive';
   } else {
     //change className back to hide menu
-    x.className = 'navbar';
+    x.className = 'navtoggle';
   }
 }
 
