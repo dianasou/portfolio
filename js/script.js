03ac6861d8d4a1323508ab7d7f1e509f1a15ff04
dianasou <dsouthiphomma@gmail.com>
2021-03-24 19:15:31 -0500

    Change navtoggle class to navbar

--- js/script.js
+++ js/script.js
@@ -6,12 +6,12 @@ console.log("This is Diana. Welcome to my portfolio!");
 // menuToggle function shows nav when clicked
 function menuToggle() {
   var x = document.getElementById('myNavtoggle');
-  if (x.className === 'navtoggle') {
+  if (x.className === 'navbar') {
     //add responsive to className to show menu
     x.className += ' responsive';
   } else {
     //change className back to hide menu
-    x.className = 'navtoggle';
+    x.className = 'navbar';
   }
 }
 
