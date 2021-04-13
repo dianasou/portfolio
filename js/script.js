04b2cf872a168822ea8268c9e9e0efe85ee94371
dianasou <dsouthiphomma@gmail.com>
2021-04-13 16:04:50 -0500

    Fix js errors

--- js/script.js
+++ js/script.js
@@ -1,38 +1,38 @@
-//personal message to readers
-console.log("This is Diana. Welcome to my portfolio!");
+// personal message to readers
+console.log('This is Diana. Welcome to my portfolio!')
 
-function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
+function menuToggle () {
+  var x = document.getElementById('myNavtoggle')
   if (x.className === 'navtoggle') {
-    //add responsive to className to show menu
-    x.className += ' responsive';
+    // add responsive to className to show menu
+    x.className += ' responsive'
   } else {
-    //change className back to hide menu
-    x.className = 'navtoggle';
+    // change className back to hide menu
+    x.className = 'navtoggle'
   }
 }
 
 // set active class to button when clicked in navbar
-const currentLocation = location.href;
-const menuItem = document.querySelectorAll(".nav-link");
+const currentLocation = location.href
+const menuItem = document.querySelectorAll('.nav-link')
 const menuLength = menuItem.length
 
 for (let i = 0; i < menuLength; i++) {
   if (menuItem[i].href === currentLocation) {
-    menuItem[i].className = "active"
+    menuItem[i].className = 'active'
   }
 }
 
 // create lightbox for thumbnail images
-const lightbox = document.createElement("div")
-lightbox.id = "lightbox"
+const lightbox = document.createElement('div')
+lightbox.id = 'lightbox'
 document.body.appendChild(lightbox)
 
-const images = document.querySelectorAll(".thumbnail")
+const images = document.querySelectorAll('.thumbnail')
 images.forEach(image => {
-  image.addEventListener("click", e => {
-    lightbox.classList.add("active")
-    const img = document.createElement("img")
+  image.addEventListener('click', e => {
+    lightbox.classList.add('active')
+    const img = document.createElement('img')
     img.src = image.src
     while (lightbox.firstChild) {
       lightbox.removeChild(lightbox.firstChild)
@@ -41,31 +41,30 @@ images.forEach(image => {
   })
 })
 
-lightbox.addEventListener("click", e => {
-  if(e.target !== e.currentTarget) return
-  lightbox.classList.remove("active")
+lightbox.addEventListener('click', e => {
+  if (e.target !== e.currentTarget) return
+  lightbox.classList.remove('active')
 })
 
 // button to go back to top
-backToTopButton = document.querySelector("#backTopBtn");
+backToTopButton = document.querySelector('#backTopBtn')
 
-window.addEventListener("scroll", scrollFunction);
+window.addEventListener('scroll', scrollFunction)
 
-function scrollFunction() {
+function scrollFunction () {
   // return distance from top
   if (window.pageYOffset > 300) {
-    //show button
-    backToTopButton.style.display = "block";
-  }
-  else {
-    //hide button
-    backToTopButton.style.display = "none";
+    // show button
+    backToTopButton.style.display = 'block'
+  } else {
+    // hide button
+    backToTopButton.style.display = 'none'
   }
 }
 
-//scroll to top when user clicks button
-backToTopButton.addEventListener("click", scrollUp);
+// scroll to top when user clicks button
+backToTopButton.addEventListener('click', scrollUp)
 
 function scrollUp () {
-  window.scrollTo(0, 0);
+  window.scrollTo(0, 0)
 }
